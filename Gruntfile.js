7de8663bcbe353b4d8cde48675f7a5ba5cd029aa
Smita Borbaruah <sborbaruah@gmail.com>
2014-02-13 13:58:05 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      all: ['Gruntfile.js', 'palis3/js/designQuickForm.js', 'palis3/test/]
+      all: ['Gruntfile.js', 'palis3/js/designQuickForm.js', 'palis3/test/QUnit.css', 'palis3/test/QUnit.js', 'palis3/test/Testing.html', 'palis3/test/Testing2.html', 'palis3/test/quickforms2Test.js']
     }
   });
 
